68753825f2441d033874dcecfbbffecc0f22189b
Alexandre Previ <alexandre.previ@gmail.com>
2021-01-24 12:18:26 +0100

    modals ok

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+import React, { useState, Fragment } from "react";
+import "./App.css";
+
+import Modal from "./Modal";
+
+const App = () => {
+  const [isModalOpened, setIsModalOpened] = useState(false);
+  const [isModalOpened2, setIsModalOpened2] = useState(false);
+
+  return (
+    <Fragment>
+      <header className="header">
+        <h1>React Modal</h1>
+      </header>
+      <div className="container">
+        <button className="btn" onClick={() => setIsModalOpened(true)}>
+          Open modal 1
+        </button>
+        <button className="btn" onClick={() => setIsModalOpened2(true)}>
+          Open modal 2
+        </button>
+      </div>
+
+      {isModalOpened && (
+        <Modal
+          title="Modal title"
+          duration={500}
+          onClose={() => setIsModalOpened(false)}
+          showCloseBtn
+        >
+          <p>Lorem ipsum dolor sit amet.</p>
+        </Modal>
+      )}
+      {isModalOpened2 && (
+        <Modal
+          title="Modal title 2"
+          duration={200}
+          onClose={() => setIsModalOpened2(false)}
+        >
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt
+            deserunt temporibus doloribus eveniet nostrum reprehenderit sequi?
+            Deserunt, eos voluptatibus laboriosam quo, expedita facere amet
+            maxime magnam soluta ab molestias dolorum dolorem beatae nisi
+            incidunt! Veniam minus nihil vitae molestias mollitia, nemo magnam
+            earum impedit odit, animi provident blanditiis repudiandae corrupti.
+          </p>
+        </Modal>
+      )}
+    </Fragment>
+  );
+};
+
+export default App;
---
+++ src/Modal.jsx
@@ -0,0 +1,79 @@
+import React, { useEffect, useRef } from "react";
+import PropTypes from "prop-types";
+
+const Modal = ({ title, children, onClose, duration = 300, showCloseBtn }) => {
+  const modal = useRef();
+  const modalBg = useRef();
+  const modalContent = useRef();
+
+  useEffect(() => {
+    document.body.style.overflow = "hidden";
+    modal.current.classList.add("disable-click");
+    modalBg.current.style.transitionDuration = duration + "ms";
+    modalContent.current.style.transitionDuration = duration + "ms";
+
+    setTimeout(() => {
+      modalBg.current.style.opacity = 0.2;
+      modalContent.current.style.opacity = 1;
+      modalContent.current.style.top = 0;
+    }, 20);
+
+    setTimeout(() => {
+      modal.current.classList.remove("disable-click");
+    }, duration + 20);
+
+    return () => {
+      document.body.style.overflow = "visible";
+    };
+  }, [duration]);
+
+  const modalCloseHandler = (e) => {
+    e.preventDefault();
+
+    modal.current.classList.add("disable-click");
+    modalBg.current.style.opacity = 0;
+    modalContent.current.style.opacity = 0;
+    modalContent.current.style.top = "-100px";
+
+    setTimeout(() => {
+      modal.current.classList.remove("disable-click");
+      onClose();
+    }, duration);
+  };
+
+  return (
+    <div className="modal" ref={modal}>
+      <div
+        className="modal__bg"
+        onClick={(e) => modalCloseHandler(e)}
+        ref={modalBg}
+      ></div>
+      <div className="modal__inner" ref={modalContent}>
+        <div className="modal__head">
+          <h2>{title}</h2>
+          {showCloseBtn && (
+            <button className="btn" onClick={(e) => modalCloseHandler(e)}>
+              &times;
+            </button>
+          )}
+        </div>
+        <div className="modal__body">{children}</div>
+        <div className="modal__foot">
+          <a href="/#" onClick={(e) => modalCloseHandler(e)}>
+            Close
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+Modal.propTypes = {
+  title: PropTypes.string.isRequired,
+  children: PropTypes.node,
+  onClose: PropTypes.func.isRequired,
+  duration: PropTypes.number,
+  showCloseBtn: PropTypes.bool,
+};
+
+export default Modal;
